f0447f3ac31d5c70bbccd89cf5fbda533c6357b5
Jeffrey Weinell <jweine2@gmail.com>
2020-10-18 18:06:07 -0500

    Update GREPmethod_RandomLociSelection_Part1_MakeHitTable_cluster.R

--- ddRAD/scripts/GREPmethod_RandomLociSelection_Part1_MakeHitTable_cluster.R
+++ ddRAD/scripts/GREPmethod_RandomLociSelection_Part1_MakeHitTable_cluster.R
@@ -1,5 +1,6 @@
-directory="/panfs/pfs.local/home/j926w878/scratch/"
-files <- list.files(path="/panfs/pfs.local/home/j926w878/scratch/Thermophis_Genome_Contigs/")
+directory="~/WorkingDirName"
+
+files <- list.files(path="~/Thermophis_Genome_Contigs/")
 
 RecognitionSeqA.pattern <- "CTGCAG"    ## PstI recognition site
 RecognitionSeqB.pattern <- "CCGG"      ## HpaII recognition site
@@ -7,8 +8,9 @@ RecognitionSeqB.pattern <- "CCGG"      ## HpaII recognition site
 RecognitionSeqA.length <- nchar(RecognitionSeqA.pattern) ### nucleotide length of Recognition Sequence A
 RecognitionSeqB.length <- nchar(RecognitionSeqB.pattern) ### nucleotide length of Recognition Sequence B
 
-outfile1 <- paste(directory,RecognitionSeqA.pattern,"_RecognitionSeqATable.txt",sep="")
-outfile2 <- paste(directory,RecognitionSeqB.pattern,"_RecognitionSeqBTable.txt",sep="")
+### Name of output files
+outfile1 <- paste0(directory,RecognitionSeqA.pattern,"_RecognitionSeqATable.txt")
+outfile2 <- paste0(directory,RecognitionSeqB.pattern,"_RecognitionSeqBTable.txt")
 
 
 for(i in 1:length(files)){
@@ -17,15 +19,15 @@ for(i in 1:length(files)){
 		write.table(headers,file=outfile1 ,sep="\t",append=F,col.names=F,row.names=F,quote=F)
 		write.table(headers,file=outfile2 ,sep="\t",append=F,col.names=F,row.names=F,quote=F)
 	}
-	accession <- gsub(".fasta","",files[i])
-	filename.temp <- paste(directory,files[i],sep="")
-	sequences <- read.table(file=filename.temp,header=T,sep="\t")
+	accession     <- gsub(".fasta","",files[i]) ### accession ID of ith contig 
+	filename.temp <- paste0(directory,files[i]) ### full filepath to ith contig
+	sequences     <- read.table(file=filename.temp,header=T,sep="\t")
 	sequence.temp <- paste(as.character(unlist(sequences)),collapse="")
 
-	RecognitionSeqA.start.pos <- unlist(gregexpr(RecognitionSeqA.pattern, sequence.temp))			## start positions of Recognition Sequence A relative to query sequence
-	RecognitionSeqA.end.pos <- RecognitionSeqA.start.pos+RecognitionSeqA.length-1					## end positions of Recognition Sequence A relative to query sequence
-	RecognitionSeqB.start.pos <- unlist(gregexpr(RecognitionSeqB.pattern, sequence.temp))           ## start positions of Recognition Sequence B relative to query sequence
-	RecognitionSeqB.end.pos <- RecognitionSeqB.start.pos+RecognitionSeqB.length-1                   ## end positions of Recognition Sequence B relative to query sequence
+	RecognitionSeqA.start.pos <- unlist(gregexpr(RecognitionSeqA.pattern, sequence.temp))		## start positions of Recognition Sequence A relative to query sequence (ith contig)
+	RecognitionSeqA.end.pos   <- RecognitionSeqA.start.pos+RecognitionSeqA.length-1			## end positions of Recognition Sequence A relative to query sequence (ith contig)
+	RecognitionSeqB.start.pos <- unlist(gregexpr(RecognitionSeqB.pattern, sequence.temp))           ## start positions of Recognition Sequence B relative to query sequence (ith contig)
+	RecognitionSeqB.end.pos   <- RecognitionSeqB.start.pos+RecognitionSeqB.length-1                 ## end positions of Recognition Sequence B relative to query sequence (ith contig)
 	
 	RecognitionSeqA.data <- cbind(accession,RecognitionSeqA.start.pos,RecognitionSeqA.end.pos)      ## a vector containing the info for Recognition Sequence A hits relative to the query sequence
 	RecognitionSeqB.data <- cbind(accession,RecognitionSeqB.start.pos,RecognitionSeqB.end.pos)      
