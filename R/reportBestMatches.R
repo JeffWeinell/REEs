cfc64c47b8677a264fc59c1b27a6ce1b5a38d579
Jeffrey Weinell <jweine2@gmail.com>
2021-02-04 20:28:59 -0600

    modified functions

--- R/reportBestMatches.R
+++ R/reportBestMatches.R
@@ -13,44 +13,28 @@
 #' @param min.bitscore.difference The difference between the best and second best matches' bitscore must be greater than this value to keep matches for the query sequence. Default = 0. This is useful for removing loci with putatitive recent duplicates in the genome.
 #' @return A data.table object containing the best matche to each input query in the input blast table. If output.table.path argument is a path (character string), then the function also writes the output as a tab-separated file.
 #' @export reportBestMatches
-reportBestMatches <- function(input.table, output.table.path=NULL, remove.subseq.matches=F, min.bitscore=50, min.bitscore.difference=0){
+reportBestMatches <- function(input.table, output.table.path=NULL, remove.subseq.matches=T, min.bitscore=50, min.bitscore.difference=0){
 	if("data.frame" %in% class(input.table)){
 		all.matches <- data.table::as.data.table(input.table)
 	}
-	### trying this out...
+	### Alternatively, could use data.frame class instead of data.table
 #	if("data.table" %in% class(input.table)){
 #		all.matches <- as.data.frame(input.table)
 #	}
-
 	if("character" %in% class(input.table)){
 		all.matches       <- data.table::fread(input=input.table,sep="\t")
 	}
-
 	colnames(all.matches) <- c("qseqid","sseqid","pident","length","mismatch","gapopen","qstart","qend","sstart","send","evalue","bitscore")
-	#print("check 0")
 	### Filter matches with bitscore less than min.bitscore
 	if(any(all.matches$bitscore < min.bitscore)){
 		filtered.matches <- all.matches[-which(all.matches$bitscore < min.bitscore),]
 	} else {
 		filtered.matches <- all.matches
 	}
-	
-#	# New method to sort rows. Need to verify that this produced the same result as the old method. This should be slightly faster though.
-#	all.matches.ordered  <- all.matches[with(all.matches, order(qseqid, bitscore, decreasing=T)),]
-
-	# New method to sort rows. Need to verify that this produced the same result as the old method. This should be slightly faster though.
+	# Sort rows, first by decreasing qseqid and then by decreasing bitscore
 	matches.ordered  <- filtered.matches[with(filtered.matches, order(filtered.matches$qseqid, filtered.matches$bitscore, decreasing=T)),]
-#	matches.ordered  <- filtered.matches[with(filtered.matches, order(qseqid, bitscore, decreasing=T)),]
-#	query.subject.sstart.send <- cbind(matches.ordered$qseqid, matches.ordered$sseqid, matches.ordered$sstart, matches.ordered$send)
-	#print("check 1")
 	# Remove rows (matches) if the match is a subsequence of another match.
 	if(remove.subseq.matches){
-		#sstart.send    <- cbind(matches.ordered$sstart, matches.ordered$send)
-		#min.vals       <- apply(X=sstart.send,MARGIN=1,FUN=min)
-		#max.vals       <- apply(X=sstart.send,MARGIN=1,FUN=max)
-		#subject.ranges <- IRanges(start=min.vals, end= max.vals, names= paste(matches.ordered$qseqid, matches.ordered$sseqid,sep="_"))
-		###
-		#query.subject       <- paste(matches.ordered$qseqid,matches.ordered$sseqid,sep=".")
 		matches.list        <- split(matches.ordered,by=c("qseqid","sseqid"))
 		min.list            <- lapply(matches.list,FUN=function(x){apply(X=x[,c("sstart","send")],MARGIN=1,FUN=min)})
 		max.list            <- lapply(matches.list,FUN=function(x){apply(X=x[,c("sstart","send")],MARGIN=1,FUN=max)})
@@ -71,61 +55,9 @@ reportBestMatches <- function(input.table, output.table.path=NULL, remove.subseq
 		}
 		filtered.matches.ordered           <- data.table::as.data.table(do.call(cbind,filtered.matches.ordered.temp2))
 		colnames(filtered.matches.ordered) <- colnames(matches.ordered)
-		#filtered.matches.ordered      <- data.table::data.table(unlist(filtered.matches.ordered.temp[,1]),unlist(filtered.matches.ordered.temp[,2]),unlist(filtered.matches.ordered.temp[,3]))
-		#remove.ID           <- unlist(remove.rows.list)
-		#query.names         <- gsub("\\.1[0-9]+$","\\.1",names(remove.ID))
-		#query.vals          <- as.numeric(remove.ID)
-		#names.vals.mat      <- data.frame(query.names,query.vals)
-		
-		#rows.indices.list   <- lapply(query.names,FUN=function(x){which(query.subject %in% x)})
-		#remove.rows.indices <- mapply(FUN=function(A,B){A[B]},A=rows.indices.list,B=query.vals)
-		
-		### The next line takes a while to run. It holds the indices of the rows to drop of matches.ordered.
-		# remove.rows.indices <- mapply(FUN=function(A,B){grep(A,query.subject)[B]},A=query.names,B=query.vals)
-
-		### Possible alternative to remove.rows.indices
-		# remove.rows.indices <- apply(X=names.vals.mat,MARGIN=1,FUN=function(A){grep(A[1],query.subject)[A[2]]})
-		#test.ranges  <- IRanges(start=min.list[[1]],end=max.list[[1]])
-		#if(length(findOverlaps(test.ranges,drop.self=T,type="within"))>0){
-		#	test.ranges2 <- test.ranges[-unique(as.matrix(findOverlaps(test.ranges,drop.self=T,type="within"))[,1])]
-		#} else {
-		#	test.ranges2 <- test.ranges
-		#}
-
-		### Unique combination of the query sequences and subject contigs in the hit table
-#		distinct.mat <- dplyr::distinct(filtered.matches[,c(1:2)]) 
-#		result       <- list(); length(result)=nrow(matches.ordered)
-#		print("filtering subsequences")
-#		pb = txtProgressBar(min = 1, max = nrow(distinct.mat), initial = 0)
-#		for(i in 1:nrow(distinct.mat)){
-#			matches.temp       <- matches.ordered[which(matches.ordered$qseqid==distinct.mat$qseqid[i] & matches.ordered$sseqid== distinct.mat$sseqid[i]),]
-#			matches.temp.mins  <- apply(X=matches.temp[,c("sstart","send")],MARGIN=1,FUN=min)
-#			matches.temp.maxes <- apply(X=matches.temp[,c("sstart","send")],MARGIN=1,FUN=max)
-#			for(k in 1:nrow(matches.temp)){
-#				#r1.temp                <- c(matches.temp.mins[k],matches.temp.maxes[k])
-#				result.index           <- which(matches.ordered$qseqid==matches.temp$qseqid[k] & matches.ordered$sseqid== matches.temp$sseqid[k] & matches.ordered$sstart == matches.temp$sstart[k] & matches.ordered$send == matches.temp$send[k])
-#				#result[result.index]   <- any(apply(X=Ophiophagus.hannah.50hits[-11,c("sstart","send")],MARGIN=1,FUN=is.subset,r1=Ophiophagus.hannah.50hits[11,c("sstart","send")]))
-#				result[result.index]   <- any(apply(X=matches.temp[-k,c("sstart","send")],MARGIN=1,FUN=is.subset,r1=matches.temp[k,c("sstart","send")]))
-#			}
-#		setTxtProgressBar(pb,i)
-#		}
-#		result <- unlist(result)
-#		if(any(result)){
-#			matches.ordered <- matches.ordered[!result,]
-#		}
-		#filtered.matches.ordered <- matches.ordered[-remove.rows.indices,]
 	} else {
 		filtered.matches.ordered <- matches.ordered
 	}
-#	#print("check 2")
-#	### Filter matches with bitscore less than min.bitscore
-#	if(any(all.matches.ordered$bitscore < min.bitscore)){
-#		filtered.matches.ordered <- all.matches.ordered[-which(all.matches.ordered$bitscore < min.bitscore)]
-#	} else {
-#		filtered.matches.ordered <- all.matches.ordered
-#	}
-	
-	#print("check 3")
 	### Report the best and second best matches.
 	best.matches          <- as.numeric(match(unique(filtered.matches.ordered$qseqid), filtered.matches.ordered$qseqid))
 	best.data             <- filtered.matches.ordered[best.matches,]
@@ -133,18 +65,15 @@ reportBestMatches <- function(input.table, output.table.path=NULL, remove.subseq
 		without.best.data     <- filtered.matches.ordered[-best.matches,]
 		second.best.matches   <- as.numeric(match(unique(filtered.matches.ordered$qseqid), without.best.data$qseqid))
 		second.best.data      <- without.best.data[second.best.matches]
-		#print("check 4")
 		#### If a query does not have a second best match, then a bitscore of zero is used for an dummy second match. There is no consequence for having a single strong match.
 		if(any(is.na(second.best.matches))){
 			second.best.data$bitscore[which(is.na(second.best.matches))] <- 0
 		}
-		#print("check 5")
 		bitscore.difference             <- as.numeric(best.data$bitscore)-as.numeric(second.best.data$bitscore)
 		if(any(bitscore.difference < min.bitscore.difference)){
 			best.data <- best.data[-which(bitscore.difference < min.bitscore.difference)]
 		}
 	}
-	#print("check 6")
 	if(!is.null(output.table.path)){
 		write.table(best.data,file=output.table.path,sep="\t",row.names=F,col.names=T,quote=F)
 	}
