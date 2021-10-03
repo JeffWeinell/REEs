b7e1fe209f861bafcfcceb753fc8a9cb3cc1bd83
Jeffrey Weinell <jweine2@gmail.com>
2021-10-03 11:28:17 -0500

    updated function

--- R/maf2fasta.R
+++ R/maf2fasta.R
@@ -5,12 +5,12 @@
 #' @param maf Path to the input MAF file
 #' @param outdir Path to directory where output fasta files should be written
 #' @param samples Names of samples to keep. Default NULL means all samples are kept.
-#' @param minwidth Number indicating the minimum alignment width to retain an alignment. Default 40.
+#' @param minwidth Number indicating the minimum alignment width to retain an alignment. Default 100.
 #' @param minsamples Number indicating the minimum number of individuals in an alignment to keep the alignment. Default 4.
 #' @param silent Logical indicating whether or not to omit printing to screen. Default TRUE.
 #' @return NULL; alignments are written to outdir
 #' @export maf2fasta
-maf2fasta <- function(maf,outdir,samples=NULL,minwidth=40,minsamples=4,silent=F){
+maf2fasta <- function(maf,outdir,samples=NULL,minwidth=100,minsamples=4,silent=F){
 	data.lines  <- readLines(maf)
 	lines.a     <- grep("^a$",data.lines)
 	lines.s1    <- lines.a+1
@@ -18,14 +18,14 @@ maf2fasta <- function(maf,outdir,samples=NULL,minwidth=40,minsamples=4,silent=F)
 	# for(i in 1:length(lines.a)){
 	for(i in 10001:length(lines.a)){
 		lines.temp  <- data.lines[lines.s1[i]:lines.slast[i]]
-		mat.temp    <- do.call(rbind,strsplit(lines.temp,split="\t"))[,-1]
-		idmat.temp  <- do.call(rbind,strsplit(mat.temp[,1],split="\\."))
-		mat.temp2   <- cbind(idmat.temp,mat.temp[,-1])
+		mat.temp    <- do.call(rbind,strsplit(lines.temp,split="\t"))[,-1,drop=F]
+		idmat.temp  <- do.call(rbind,strsplit(mat.temp[,1,drop=F],split="\\."))
+		mat.temp2   <- cbind(idmat.temp,mat.temp[,-1,drop=F])
 		colnames(mat.temp2) <- c("SampleName","SampleContig","SourceStart","SourceEnd","SourceStrand","SourceSize","Sequence")
 		if(!is.null(samples)){
 			#match(samples,mat.temp2[,"SampleName"])
 			if(any(mat.temp2[,"SampleName"] %in% samples)){
-				mat.temp3 <- mat.temp2[which(mat.temp2[,"SampleName"] %in% samples),]
+				mat.temp3 <- mat.temp2[which(mat.temp2[,"SampleName"] %in% samples),,drop=F]
 			} else {
 				next
 			}
@@ -45,13 +45,6 @@ maf2fasta <- function(maf,outdir,samples=NULL,minwidth=40,minsamples=4,silent=F)
 		print(i)
 	}
 }
-
-# maf="/panfs/pfs.local/home/j926w878/scratch/scratch_v3/SequenceCapture/SnakeCap_AllSamples/Cactus/TestSnakesSubset.maf"
-# outdir  <- "/panfs/pfs.local/home/j926w878/scratch/scratch_v3/SequenceCapture/SnakeCap_AllSamples/Cactus/test"
-# samples <- c("Ahaetulla","Aplopeltura","Boiga","Elapsoidea","Hemibungarus")
-# minwidth=100
-# minsamples=2
-
 
 
 
