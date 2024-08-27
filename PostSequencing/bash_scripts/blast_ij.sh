af1ff056517c0674151e504ae9ccccff78141cb4
JeffWeinell <jweine2@gmail.com>
2024-08-27 18:08:11 -0400

    removed some tree files and updated README.md

--- PostSequencing/bash_scripts/blast_ij.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-module load 'blast+/2.9.0'
-#blastn -task $1 -db $2 -query $3 -out $4 -max_hsps $5 -evalue $6 -outfmt $7 -num_threads $8 -word_size $9 -gapopen $10 -gapextend $11 -reward $12 -penalty $13 -perc_identity $14
-#blastn -task 'blastn' -db "$1" -query "$2" -out "$3" -max_hsps 1 -evalue 0.01 -outfmt 6 -num_threads 10 -word_size 10 -gapopen 5 -gapextend 2 -reward 2 -penalty '-3' -perc_identity 0
-echo $1"_"$2
-blastn -task 'blastn' -db "$1" -query "$2" -out "$3" -max_hsps 1 -evalue 0.0000000001 -outfmt 6 -num_threads 10 -word_size 100 -gapopen 5 -gapextend 2 -reward 2 -penalty '-3' -perc_identity 0
--- PostSequencing/bash_scripts/test3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-module load R
-
-BLASTDBDIR=$1
-VAR=$2
-VAR2=$3
-
-Rscript '/panfs/pfs.local/home/j926w878/work/conda/snakecap/test3.R' "$BLASTDBDIR" "$VAR" "$VAR2"
